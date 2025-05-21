367d2f99d64295d281d150c806bcd4b8cc03bf27
Marie Edet <marie.edet@le-campus-numerique.fr>
2025-05-21 12:07:34 +0200

    sort error corrected and variable renamed

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -60,7 +60,7 @@ print('\n==================================================================\n')
 
 
 # Sort data ratings by created field (groupby + lambda function + sorted)
-sortRatingsField = cloneDF(mergeRatings)
-sortRatingsField = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
-    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort('COUNT', ascending=False)
-print('My info sorted: \n%s' % sortRatingsField[:15])
+sortRatingsField_Marie = cloneDF(mergeRatings)
+sortRatingsField_Marie = sortRatingsField_Marie.groupby(['movie_id', 'title'])['rating'].agg(
+    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort_values('COUNT', ascending=False)
+print('My info sorted: \n%s' % sortRatingsField_Marie[:15])
