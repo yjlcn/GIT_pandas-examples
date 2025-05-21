f30072492aed9d65e68543d252028816a3528c22
Yoann Julliard <yoann.julliard@le-campus-numerique.fr>
2025-05-21 12:02:36 +0200

    changed sorting rating name and debugged the sort using the new func

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -60,7 +60,7 @@ print('\n==================================================================\n')
 
 
 # Sort data ratings by created field (groupby + lambda function + sorted)
-sortRatingsField = cloneDF(mergeRatings)
-sortRatingsField = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
-    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort('COUNT', ascending=False)
-print('My info sorted: \n%s' % sortRatingsField[:15])
+sortRatingsField_Yoann = cloneDF(mergeRatings)
+sortRatingsField_Yoann = sortRatingsField_Yoann.groupby(['movie_id', 'title'])['rating'].agg(
+    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort_values('COUNT', ascending=False)
+print('My info sorted: \n%s' % sortRatingsField_Yoann[:15])
