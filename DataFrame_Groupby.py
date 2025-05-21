2882c4dec4a72035f50bbda8ee4ba3e23cb5678c
Yoann Julliard <yoann.julliard@le-campus-numerique.fr>
2025-05-21 12:39:08 +0200

    changing back the var name to generate again a conflict

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -60,7 +60,7 @@ print('\n==================================================================\n')
 
 
 # Sort data ratings by created field (groupby + lambda function + sorted)
-sortRatingsField_Marie = cloneDF(mergeRatings)
-sortRatingsField_Marie = sortRatingsField_Marie.groupby(['movie_id', 'title'])['rating'].agg(
+sortRatingsField_YoannIsBack = cloneDF(mergeRatings)
+sortRatingsField_YoannIsBack = sortRatingsField_YoannIsBack.groupby(['movie_id', 'title'])['rating'].agg(
     COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort_values('COUNT', ascending=False)
-print('My info sorted: \n%s' % sortRatingsField_Marie[:15])
+print('My info sorted: \n%s' % sortRatingsField_YoannIsBack[:15])
