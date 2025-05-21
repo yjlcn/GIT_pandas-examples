60a307868d7066ba82c79d9bb72908b5ff79618d
Xavier Bednarek <xavier.bednarek@le-campus-numerique.fr>
2025-05-21 12:11:54 +0200

    Patch pour générer un conflit !

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -61,6 +61,6 @@ print('\n==================================================================\n')
 
 # Sort data ratings by created field (groupby + lambda function + sorted)
 sortRatingsField = cloneDF(mergeRatings)
-sortRatingsField = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
-    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort('COUNT', ascending=False)
+sortRatingsField_coucou = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
+    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort_values('COUNT', ascending=False)
 print('My info sorted: \n%s' % sortRatingsField[:15])
